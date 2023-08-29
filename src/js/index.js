6154b1b76094845f57180b46cfdb580015dd3136
João Paulo Lopes Pinho <joao.pinho@estudante.ifms.edu.br>
2023-08-28 22:22:34 -0400

    feature: listagem de atendimento

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,31 @@
 import "./libs"
-const inputNome  = document.querySelector('#input-nome')
-const selectPrioridade  = document.querySelector('#select-prioridade')
-const btnIncluir  = document.querySelector('#btn-incluir')
-const ulLista  = document.querySelector('#ul-lista')
-const displayNome  = document.querySelector('#display-nome')
-const btnChamar  = document.querySelector('#btn-chamar')
-const listaDeAtendimento  = new Array()
+
+const inputNome = document.querySelector('#input-nome');
+const selectPrioridade = document.querySelector('#select-prioridade');
+const btnIncluir = document.querySelector('#btn-incluir');
+const ulLista = document.querySelector('#ul-lista');
+const displayNome = document.querySelector('#display-nome');
+const btnChamar = document.querySelector('#btn-chamar');
+
+
+const listaDeAtendimento = new Array();
 
 function incluir(){
-    listaDeAtendimento.push(inputNome.value)
-    listar()
+
+    listaDeAtendimento.push(inputNome.value);
+    listar();
 }
 
+
 function listar(){
+    ulLista.innerHTML='';
     for(let nome of listaDeAtendimento){
-        console.log(nome)
+            let li = document.createElement('li')
+            li.classList.add('list-group-item')
+            li.textContent = nome
+            ulLista.appendChild(li)
+        }
     }
-}
-//eventos
+
+//Eventos
 btnIncluir.onclick = incluir
